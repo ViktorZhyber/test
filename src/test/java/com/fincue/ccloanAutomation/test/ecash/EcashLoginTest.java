aa1d3dde27e9e20c796cd41e845b595be6d28533
ViktorZhyber <zhyber.viktor@gmail.com>
2018-07-06 23:48:26 +0300

    add annotation categories

--- src/test/java/com/fincue/ccloanAutomation/test/ecash/EcashLoginTest.java
+++ src/test/java/com/fincue/ccloanAutomation/test/ecash/EcashLoginTest.java
@@ -3,6 +3,7 @@ package com.fincue.ccloanAutomation.test.ecash;
 import com.fincue.ccloanAutomation.core.BaseConfigurationTest;
 import com.fincue.ccloanAutomation.pages.ecash.Login;
 import com.fincue.ccloanAutomation.pages.ecash.UserInformation;
+import com.fincue.ccloanAutomation.test.categories.EcashAcceptance;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import ru.yandex.qatools.allure.annotations.Features;
@@ -12,7 +13,7 @@ import static com.fincue.ccloanAutomation.testdata.TestData.*;
 public class EcashLoginTest extends BaseConfigurationTest {
 
     @Features("Login to e-cash")
-    @Category(EcashLoginTest.class)
+    @Category(EcashAcceptance.class)
     @Test
     public void testLogin(){
         //Step;
